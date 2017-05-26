cecd4f268329d909671717d26273e9b89a59b200
thongle <lpthong90@gmail.com>
2017-05-27 00:46:20 +0700

    add: pay + receive + repay_amount

--- agents/bank.py
+++ agents/bank.py
@@ -1,32 +1,33 @@
 import numpy as np
 
 from mesa import Agent
+import model.fomulas as fl
 
 class Bank(Agent):
     def __init__(self, params):
         self.code = params['code'] if 'code' in params else ''
         self.name = params['name'] if 'name' in params else ''
 
-    def step(self, stage):
+    def step(self, stage, banks):
         """ A single step of the agent. """
         if stage in [1, 2, 3]:
             return {
-                1 : self.stage_1,
-                2 : self.stage_2,
-                3 : self.stage_3
-            }[stage]()
+                1: self.stage_1,
+                2: self.stage_2,
+                3: self.stage_3
+            }[stage](banks)
 
-    def pay(self):
-        print "pay"
+    def pay(self, amount):
+        print "---> " + 'Code: ' + str(self.code) + " - Name: " + self.name + " : " + "paid" + " " + str(amount)
 
-    def receive(self):
-        print "receive"
+    def receive(self, amount):
+        print "---> " + 'Code: ' + str(self.code) + " - Name: " + self.name + " : " + "received" + " " + str(amount)
 
-    def lend(self):
-        print "lend"
+    def lend(self, amount):
+        print "---> " + 'Code: ' + str(self.code) + " - Name: " + self.name + " : " + "lend" + " " + str(amount)
 
-    def borrow(self):
-        print "borrow"
+    def borrow(self, amount):
+        print "---> " + 'Code: ' + str(self.code) + " - Name: " + self.name + " : " + "borrowed" + " " + str(amount)
 
     def change_deposit(self):
         print "change_deposit"
@@ -40,19 +41,26 @@ class Bank(Agent):
     def bankrupt(self):
         print "bankrupt"
 
-    def stage_1(self):
+    def stage_1(self, banks):
         '''
         A model step. At stage 1, borrowing banks repay part of their loan to lending banks
         '''
+
+        for bank in banks:
+            if bank.code != self.code:
+                amount = fl.repay_amount(s=0, b=0)
+                self.pay(amount)
+                bank.receive(amount)
+
         print 'Code: ' + str(self.code) + " - Name: " + self.name + " --> Run " + "stage_1"
 
-    def stage_2(self):
+    def stage_2(self, banks):
         '''
         At stage 2, banks borrow and lend in the interbank market
         '''
         print 'Code: ' + str(self.code) + " - Name: " + self.name + " --> Run " + "stage_2"
 
-    def stage_3(self):
+    def stage_3(self, banks):
         '''
         At stage 3, banks update other entries of the balance sheet
         '''
--- model/fomulas.py
+++ model/fomulas.py
@@ -1,8 +1,10 @@
 import random
 import numpy as np
 
-def repay_amount():
-    amount = 0
+# s(st_ratio)
+# b(borrowing_amount)
+def repay_amount(s = 0, b = 0):
+    amount = b * np.dot([s, 1 - s], [np.random.uniform(0.95, 1), np.random.uniform(0.2, 1)])
     return amount
 
 def size_score():
